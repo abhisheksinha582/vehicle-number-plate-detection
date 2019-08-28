4c9a5a086f37a6c12aa809aad2061ad1f4b56465
abhisheksinha582 <47332311+abhisheksinha582@users.noreply.github.com>
2019-08-28 19:13:16 +0530

    Update recognize_plate.py

--- recognize_plate.py
+++ recognize_plate.py
@@ -2,17 +2,28 @@ import pytesseract
 import os
 
 
-def recognize_plate(plate):
+def Recognize_plate(plate):
 
 	pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #Enter your path where tesseract-ocr is installed
 
 	curr_dir = os.getcwd()
+	
+	#converting the image of cropped plate to grayscale
+	plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+	
+	#showing the grayscale image of plate, press 0 to continue when the image pops up...
+	cv2.imshow('gray_plate', plate)
+	cv2.waitKey(0)
 
-	#Use tesseract to convert image into string
-	text = pytesseract.image_to_string(cropped_img_loc, lang='eng')
+	#Use tesseract to convert number in theimage into string by using its OCR Engine
+	text = pytesseract.image_to_string(plate, lang='eng')
 	print("Number is : ", text)
-
-	plate_number = open(curr_dir + "/plate_number.txt","w") 
-
+	
+	#creating aand opening a text file to write the license plate number
+	plate_number = open(curr_dir + "/plate_number.txt","w")
+	
+	#writing the license plate number into the text file.
 	plate_number.write(text)
+	
+	#finally we need to close the text file.
 	plate_number.close()
